2f092d67f6380722757b90d82d2b1a49d4248258
Pavlo Oliinyk <pavlo25092000@gmail.com>
2020-10-30 12:21:18 +0200

    First commit

---
+++ Credit_Relationship/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using Credit_Relationship.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Credit_Relationship.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        CreditContext db = new CreditContext(); 
+        public ActionResult Index() {
+            var orders = db.Orders.Include(p => p.Credit);
+            
+            return View(orders.ToList()); 
+        }
+
+        public ActionResult ListCredits()
+        {
+            return View(db.Credits);
+        }
+
+        public ActionResult CreditDetails(int? id) { 
+            if (id == null) { 
+                return HttpNotFound(); 
+            } 
+            Credit credit = db.Credits.Find(id); 
+            if (credit == null) { 
+                return HttpNotFound(); 
+            } 
+            credit.Orders = db.Orders.Where(m => m.CreditId == credit.Id); 
+            return View(credit); 
+        }
+
+        //Edit Order
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Order order = db.Orders.Find(id);
+            if (order != null)
+            {
+                SelectList credits = new SelectList(db.Credits, "Id", "Type", order.CreditId); 
+                ViewBag.Credits = credits; 
+                return View(order);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Order order)
+        {
+            db.Entry(order).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Create Order
+        [HttpGet] 
+        public ActionResult Create() {
+            SelectList credits = new SelectList(db.Credits, "Id", "Type"); 
+            ViewBag.Credits = credits;
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult Create(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Delete Order
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Order b = db.Orders.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            Credit credit = db.Credits.Find(b.CreditId);
+            ViewBag.Credits = credit;
+            return View(b);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Order b = db.Orders.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            db.Orders.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Credit_Relationship/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Credit_Relationship.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Person { get; set; }
+        public string Address { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+        public int Days { get; set; }
+        public int? CreditId { get; set; }
+        public Credit Credit { get; set; }
+    }
+}
